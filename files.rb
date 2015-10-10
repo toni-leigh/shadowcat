7f36effecf27c759baba38966a1a61e386ff58b4
Toni Leigh Sharpe <toni.leigh@sage.com>
2015-10-10 12:07:10 +0100

    comment out for safety

--- files.rb
+++ files.rb
@@ -30,58 +30,58 @@ end
 #   sass_file.close
 # end
 
-[
-  'navigation',
-  'heading',
-  'video-spotlight',
-  'text',
-  'aside',
-  'video',
-  'testimonials',
-  'image-fixed',
-  'button-pair',
-  'choice-projects',
-  'call-to-action',
-  'blog-small',
-  'contact',
-  'footer',
-  'thumbnail-gallery',
-  'lightbox'
-].each do |f|
-  Dir.mkdir("src/sections/#{f}")
-  js_file = File.new(js_path('sections',f),'w')
-  js_file.puts(react_class(f))
-  js_file.close
-  sass_file = File.new(sass_path('sections',f),'w')
-  sass_file.puts(sass(f))
-  sass_file.close
-end
+# [
+#   'navigation',
+#   'heading',
+#   'video-spotlight',
+#   'text',
+#   'aside',
+#   'video',
+#   'testimonials',
+#   'image-fixed',
+#   'button-pair',
+#   'choice-projects',
+#   'call-to-action',
+#   'blog-small',
+#   'contact',
+#   'footer',
+#   'thumbnail-gallery',
+#   'lightbox'
+# ].each do |f|
+#   Dir.mkdir("src/sections/#{f}")
+#   js_file = File.new(js_path('sections',f),'w')
+#   js_file.puts(react_class(f))
+#   js_file.close
+#   sass_file = File.new(sass_path('sections',f),'w')
+#   sass_file.puts(sass(f))
+#   sass_file.close
+# end
 
-[
-  'logo',
-  'page-heading',
-  'bread-crumbs',
-  'key-value-detail',
-  'section-heading-simple',
-  'section-heading-with-strapline',
-  'button-large',
-  'button-small',
-  'button-cta',
-  'button-submit',
-  'input-text',
-  'input-textarea',
-  'contact-detail',
-  'map',
-  'aside-panel',
-  'image-thumbnail'
-].each do |f|
-  Dir.mkdir("src/modules/#{f}")
-  js_file = File.new(js_path('modules',f),'w')
-  js_file.puts(react_class(f))
-  js_file.close
-  sass_file = File.new(sass_path('modules',f),'w')
-  sass_file.puts(sass(f))
-  sass_file.close
-end
+# [
+#   'logo',
+#   'page-heading',
+#   'bread-crumbs',
+#   'key-value-detail',
+#   'section-heading-simple',
+#   'section-heading-with-strapline',
+#   'button-large',
+#   'button-small',
+#   'button-cta',
+#   'button-submit',
+#   'input-text',
+#   'input-textarea',
+#   'contact-detail',
+#   'map',
+#   'aside-panel',
+#   'image-thumbnail'
+# ].each do |f|
+#   Dir.mkdir("src/modules/#{f}")
+#   js_file = File.new(js_path('modules',f),'w')
+#   js_file.puts(react_class(f))
+#   js_file.close
+#   sass_file = File.new(sass_path('modules',f),'w')
+#   sass_file.puts(sass(f))
+#   sass_file.close
+# end
 
 
