6a49dab226e9d5a38e1e38c4453e0abf9ffdf9cb
Toni Leigh Sharpe <toni.leigh@sage.com>
2015-10-10 12:06:42 +0100

    files handles all areas

--- files.rb
+++ files.rb
@@ -15,17 +15,71 @@ def sass_path(folder, key)
   "src/#{folder}/#{key}/_#{key}.scss"
 end
 
+# [
+#   'service',
+#   'project',
+#   'blog',
+#   'blog-post'
+# ].each do |f|
+#   Dir.mkdir("src/pages/#{f}")
+#   js_file = File.new(js_path('pages',f),'w')
+#   js_file.puts(react_class(f))
+#   js_file.close
+#   sass_file = File.new(sass_path('pages',f),'w')
+#   sass_file.puts(sass(f))
+#   sass_file.close
+# end
+
 [
-  'service',
-  'project',
-  'blog',
-  'blog-post'
+  'navigation',
+  'heading',
+  'video-spotlight',
+  'text',
+  'aside',
+  'video',
+  'testimonials',
+  'image-fixed',
+  'button-pair',
+  'choice-projects',
+  'call-to-action',
+  'blog-small',
+  'contact',
+  'footer',
+  'thumbnail-gallery',
+  'lightbox'
 ].each do |f|
-  Dir.mkdir("src/pages/#{f}")
-  js_file = File.new(js_path('pages',f),'w')
+  Dir.mkdir("src/sections/#{f}")
+  js_file = File.new(js_path('sections',f),'w')
   js_file.puts(react_class(f))
   js_file.close
-  sass_file = File.new(sass_path('pages',f),'w')
+  sass_file = File.new(sass_path('sections',f),'w')
+  sass_file.puts(sass(f))
+  sass_file.close
+end
+
+[
+  'logo',
+  'page-heading',
+  'bread-crumbs',
+  'key-value-detail',
+  'section-heading-simple',
+  'section-heading-with-strapline',
+  'button-large',
+  'button-small',
+  'button-cta',
+  'button-submit',
+  'input-text',
+  'input-textarea',
+  'contact-detail',
+  'map',
+  'aside-panel',
+  'image-thumbnail'
+].each do |f|
+  Dir.mkdir("src/modules/#{f}")
+  js_file = File.new(js_path('modules',f),'w')
+  js_file.puts(react_class(f))
+  js_file.close
+  sass_file = File.new(sass_path('modules',f),'w')
   sass_file.puts(sass(f))
   sass_file.close
 end
